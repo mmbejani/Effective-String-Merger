9cd3f03377ca07a60ac863236cd9667870356328
Mahdi <mohammad.mahdi.bejani@gmail.com>
2023-09-02 16:06:24 +0330

    fisrt function has been completed

---
+++ native/header/utils.hh
@@ -0,0 +1,18 @@
+#include <string>
+#include <locale>
+#include <codecvt>
+#include <pybind11/pybind11.h>
+
+using namespace std;
+
+
+namespace za {
+    template <typename T>
+    struct Pair {
+        T first;
+        T second;
+    };
+
+    string merge_string(const string&, const string&) noexcept;
+    Pair<Pair<int>> most_similar_string(const u16string&, const &) noexcept;
+}
